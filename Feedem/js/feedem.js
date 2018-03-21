842e9f368e7c5c6488074a6c77ef464a73d2fa42
Nadish Sood <nadishsood@gmail.com>
2018-03-22 03:16:06 +0530

    just open the index.html, make sure you're connected to wifi

---
+++ Feedem/js/feedem.js
@@ -0,0 +1,88 @@
+ $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  /* Event type jquery ui*/
+
+ $( function() {
+    $( "#speed" ).selectmenu();
+ 
+    $( "#files" ).selectmenu();
+ 
+    $( "#number" )
+      .selectmenu()
+      .selectmenu( "menuWidget" )
+        .addClass( "overflow" );
+ 
+    $( "#salutation" ).selectmenu();
+  } );
+
+/*<div class="row">
+                            
+                            <div class=" col-sm-6 col-sm-offset-4 form-div ">
+                                <p><a class="btn btn-lg cusBtn" id="modal-close-button" href="#" role="button" value="close popup">x</a></p>
+                                
+                                <form class="form-horizontal">
+                                   
+                                    <div class="form-group dropdown-element">
+                                        <label for="inputPassword3" class="col-sm-4 control-label">Event Type : </label>
+                                        <div class="col-sm-8">
+                                          <div class="dropdown">
+                                          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                            Wedding
+                                            <span class="caret"></span>
+                                          </button>
+                                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                            <li><a href="#">Wedding</a></li>
+                                            <li><a href="#">Engagement/ Reception etc</a></li>
+                                            <li><a href="#">Party</a></li>
+
+                                          </ul>
+                                        </div> 
+                                        </div>
+                                      </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="inputEmail3" class="col-sm-4 control-label">Name of Event :</label>
+                                        <div class="col-sm-8">
+                                          <input type="email" class="form-control inputholders" id="inputEmail3" placeholder="A weds B or Party's Name">
+                                        </div>
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="inputPassword3" class="col-sm-4 control-label">Date Of Event :</label>
+                                        <div class="col-sm-8">
+                                          <input type="text" class="form-control" id="inputPassword3" placeholder="Date of your event">
+                                        </div>
+                                      </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="inputPassword3" class="col-sm-4 control-label">Place of Event :</label>
+                                        <div class="col-sm-8">
+                                            <textarea rows="3" cols="50" type="text" class="form-control address-input" id="inputPassword3" placeholder="Complete Address please ! "></textarea>
+                                        </div>
+                                      </div>
+                                    
+                                    
+                                    
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-4 col-sm-10">
+                                          <div class="checkbox">
+                                            <label>
+                                              <input type="checkbox"> I agree to give you my leftover food !
+                                            </label>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="form-group">
+                                        <div class="col-sm-offset-4 col-sm-8">
+                                          <button type="submit" class="btn btn-default">Submit !</button>
+                                        </div>
+                                      </div>
+                                    </form>
+                            
+                            </div>
+                        </div>
+                  
+                  
+                        </div>*/
+
+
